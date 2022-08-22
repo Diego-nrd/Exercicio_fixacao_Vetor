81a0c677596b44cafe93128001b2b6259b1029b2
Diego-nrd <diego-11-0@hotmail.com>
2022-08-21 23:06:10 -0300

    Add project files.

---
+++ Exercicio_fixacao_Vetor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio_fixacao_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] quartos = new Quarto[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nAluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.Write("");
+
+                quartos[quarto] = new Quarto { Nome = nome, Email = email };
+
+            }
+            Console.WriteLine("\nQuartos Ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (quartos[i] != null)
+                {
+                    
+                    Console.WriteLine(i + ": " + quartos[i]);
+                }
+                else
+                {
+                   //Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio_fixacao_Vetor/Quarto.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio_fixacao_Vetor
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
